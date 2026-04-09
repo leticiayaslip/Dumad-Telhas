290f4cdd27a8ca8ef12209b9d8b8b765e7221f16
Leticia Alves <leticiaalves.yaslip@gmail.com>
2026-04-09 10:15:28 -0300

    alteraçoes em types

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,28 @@
+// export {};
+
+// declare global {
+//   interface Grecaptcha {
+//     ready(callback: () => void): void;
+//     execute(siteKey: string, options: { action: string }): Promise<string>;
+//     render(container: string | HTMLElement, parameters: Record<string, unknown>): void;
+//   }
+
+//   interface Window {
+//     grecaptcha?: Grecaptcha;
+//   }
+// }
+
 export {};
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
 
 declare global {
   interface Grecaptcha {
